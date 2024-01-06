9c13f4e9b73aae919295f9bd48c8dfcd9c57f3b6
Kostiantyn Lisovets <mopchop.pes@gmail.com>
2024-01-06 21:09:23 +0200

    Update accent_color.rs

--- src-tauri/src/accent_color.rs
+++ src-tauri/src/accent_color.rs
@@ -3,14 +3,21 @@ use winreg::RegKey;
 use regex::Regex;
 
 #[tauri::command]
-///returns current windows accent color in hex
+/// returns current windows accent color in hex
+///
+/// default color - 0xffd77800 (4292311040) [src](https://learn.microsoft.com/en-us/windows-hardware/customize/desktop/unattend/microsoft-windows-shell-setup-themes-windowcolor#values)
 pub fn get_accent_color() -> String{
     let hkcu = RegKey::predef(HKEY_CURRENT_USER);
-    let apps = hkcu.open_subkey("Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Accent").unwrap();
+    let path = "Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Accent";
 
-    let acm: u32 = apps.get_value("AccentColorMenu").unwrap();
+    let accent = match hkcu.open_subkey(path){
+        Ok(_) => hkcu.open_subkey(path).unwrap(),
+        Err(_error) => return String::from("#0078d7")
+    };
 
-    return process_string(format!("{:x}", acm))
+    let color: u32 = accent.get_value("AccentColorMenu").unwrap_or_else(|_| {4292311040});
+
+    return process_string(format!("{:x}", color))
 }
 ///ffd47800 -> #0078d4
 fn process_string(input: String) -> String{
