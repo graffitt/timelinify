f931e43f775377af8f0888b8e32675db2dbdbd02
Kostiantyn Lisovets <mopchop.pes@gmail.com>
2024-01-07 22:27:13 +0200

    fixed files path

--- change_version.ps1
+++ change_version.ps1
@@ -6,23 +6,23 @@ git tag
 $version = Read-Host -Prompt 'enter new version'
 
 # Cargo.toml
-$c_toml = [string]::Join("`n", (Get-Content Cargo.toml))
+$c_toml = [string]::Join("`n", (Get-Content .\src-tauri\Cargo.toml))
 
 # get previous version
 $c_toml -match '\nversion.*?"([^"]+)"' | Out-Null
 $c_toml_prev_version = $Matches.1
 
 [regex]::Replace($c_toml, '\nversion.*?"([^"]+)"', "`nversion = `"$version`"") |
-Set-Content Cargo.toml -NoNewline
+Set-Content .\src-tauri\Cargo.toml -NoNewline
 
 # get project name from Cargo.toml
 $c_toml -match 'name.*?"(?<name>[^"]+)"' | Out-Null
 $proj_name = $Matches.name
 
 # Cargo.lock
-$c_lock = [string]::Join("`n", (Get-Content Cargo.lock))
+$c_lock = [string]::Join("`n", (Get-Content .\src-tauri\Cargo.lock))
 [regex]::Replace($c_lock, "name.*`"($proj_name)`"\nversion.*?`"([^`"]+)`"", "name = `"$`1`"`nversion = `"$version`"") |
-Set-Content Cargo.lock -NoNewline
+Set-Content .\src-tauri\Cargo.lock -NoNewline
 
 # write version change
 Write-Output "$c_toml_prev_version -> $version"
