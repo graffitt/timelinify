9aab79d52f2dddd7caa585db5f40e77bf88dc6ce
Kostiantyn Lisovets <mopchop.pes@gmail.com>
2024-01-16 22:39:48 +0200

    Update unfinished_session.js

--- src/js/unfinished_session.js
+++ src/js/unfinished_session.js
@@ -2,9 +2,9 @@ const unfinished_session = async (apps_array) => {
     let file = '.timelinify/_unfinished.json'
 
     if(await invoke('file_exist', {path: '_unfinished.json'})){
-        let {id, start, end} = JSON.parse(await readTextFile(file, file_options))
+        const {id, start} = JSON.parse(await readTextFile(file, file_options))
 
-        end = JSON.parse(await invoke('get_file_metadata', {path: '_unfinished.json'})).modified
+        let end = JSON.parse(await invoke('get_file_metadata', {path: '_unfinished.json'})).modified
         end = to_iso_string(new Date(new Date(end) - 2_000))// 2_000 = 5000(file writing delay) / 2
 
         let app = apps_array.find(app => app.id === id)
